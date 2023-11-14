5bf3133bb397d616f7f93b3f65533a613e71fac7
ann-chebet <annchebet09@gmail.com>
2023-11-14 18:51:30 +0300

    adding flesh to project

--- tues/home/views.py
+++ tues/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def w(request):
-    return render(request,'a.html')
+    return render(request,'index.html')
 
 def q(request):
     return render(request,'b.html')
